4357678305cf1675ef6d38b7eadb517050a40745
malloc72p <dase1102@gmail.com>
2020-07-29 19:08:13 +0900

    변경내용 : 모듈화 후 안쓰는 모듈 import하는 코드 제거함

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { JwtStrategyService } from './Model/social-login/jwt-strategy/jwt-strategy.service';
-import { AuthService } from './Model/social-login/auth/auth.service';
-import { GoogleStrategyService } from './Model/social-login/google-strategy/google-strategy.service';
-import { PassportModule } from '@nestjs/passport';
 import { AuthCallbackController } from './Controller/auth-callback/auth-callback.controller';
 import { ProblemController } from './Controller/problem-controller/problem.controller';
 import { SectionController } from './Controller/section-controller/section.controller';
-import { ErrorHandlerService } from './Model/error-handler/error-handler.service';
-import { DiscordSessionMgrService } from './Model/session-manager/discord-session-mgr/discord-session-mgr.service';
 import { DiscordSessionController } from './Controller/discord-session/discord-session.controller';
 import { DaoModule } from './Model/DAO/dao.module';
 import { DiscordSessionMgrModule } from './Model/session-manager/discord-session-mgr/discord-session-mgr.module';
